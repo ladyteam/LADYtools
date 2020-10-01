341db9ac941f6c440da5e1fb3b4c4f5b56cd0030
ladyteam <roginovicci@hotmail.com>
2020-10-01 15:28:29 +0300

    New function ylim is added to plot_band_partial.py

--- plot_band_partial.py
+++ plot_band_partial.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 
-# Test with ./plot_band_partial.py -i init/displ.in --dim "-1 1 1  1 -1 1   1 1 -1" --band "0.375  0.375  0.75  0.0 0.0 0.0  0.5  0.5  0.5    0.5  0.25  0.75   0.500  0.000  0.500  0.0 0.0 0.0  0.5 0.5 0.5   0.625  0.25  0.625 " --band-labels 'K $\Gamma$ L W  X  $\Gamma$ L U' --atomset "1 2 -3 -4 -5 -6" --nac --calculator "abinit" --factor=716.8519 --cmap-labels "O Si" -o band_partial.pdf
+# Test with ./plot_band_part.py -i init/displ.in --dim "2 2 2" --band "1/3 1/3 0.0   1/2 0.0 0.0  0.0 0.0 0.0  0.0 0.0 0.5   1/3 1/3 1/2" --band-labels 'K  M $\Gamma$ A  H' --atomset "1 2 3 4 -5 -6 -7 -8" --nac --calculator "abinit" --factor=716.8519 --cmap-labels "AlN GaN" -o band_partial.pd
+# ./plot_band_partial.py -i init/displ.in --dim "2 2 2" --band "0.0 0.0 0.0  0.0 0.0 0.5" --band-labels '$\Gamma$ A ' --atomset "1 2 3 4 -5 -6 -7 -8" --nac --calculator "abinit" --factor=716.8519 --cmap-labels "AlN GaN" -o band_partial_G-A.pdf --color-smooth=10 --np=301 --ylim="0 950"
 
 import h5py
 import matplotlib.pyplot as plt 
 import phonopy
 from phonopy.phonon.band_structure import get_band_qpoints_and_path_connections
-#from phonopy.phonon.band_structure import get_band_structure_dict
 import argparse
 import warnings
 import matplotlib
@@ -43,20 +43,9 @@ def displstrength(eig,atomsetp,atomsetn,masses):
         ennorm=0
     else:
         ennorm=np.linalg.norm(en)
-#    print(e)
-#    print(ep)
-#    print(en)
-#    print("Full Norm= %f; ep=%f; en=%f" % (enorm, epnorm, ennorm))
-#    print('Ratio =%f' % (epnorm/enorm-ennorm/enorm))
     return(epnorm/enorm-ennorm/enorm)
-#        print("%s" % "".join("% 10.8f " % (eig[i*3+j]/sqrt(masses[i])) for j in range(3)))
 
-
-
-
-parser = argparse.ArgumentParser(description='The program is to plot partial bandstructure')
-
-
+parser = argparse.ArgumentParser(description='The program plots partial bandstructure')
 parser.add_argument("-i", "--input", action="store", type=str, dest="in_fn", help="Input filename")
 parser.add_argument("-o", "--output", action="store", type=str, dest="out_fn", help="Output filename")
 parser.add_argument("--calculator", action="store", type=str, dest="calc", help="Ab initio program (vasp, abinit, crystal)")
@@ -72,10 +61,9 @@ parser.add_argument("--atomset", action="store", type=str, dest="atset",
 parser.add_argument("--np", action="store", type=int, dest="npoints", default=51, help="Number of points for interpalation. Default is 51")
 parser.add_argument('--nac', dest='nac', action='store_true')
 parser.add_argument('--color-smooth', dest='cmapintpnp', default=0, type=int, help="Window length of Savitzky-Golay filter for smoothing of the colormap artifacts")
-
+parser.add_argument("--ylim", action="store", type=str, dest="ylim", help="Limits for y axis")
 
 args = parser.parse_args()
-
 
 if (args.in_fn == None):
     print('Error. No input filename was given.')
@@ -120,16 +108,13 @@ for t in args.labels.split():
 print(pm)
 print(sc)
 
-print(args.nac)
-
 ph = phonopy.load(supercell_matrix=sc,
                   primitive_matrix=pm,
                   unitcell_filename=args.in_fn,
                   is_nac=args.nac,  calculator=args.calc, factor=args.factor,
                   force_sets_filename=args.fsetfn)
 
-print(ph.primitive.get_masses())
-
+print(ph.primitive)
 
 qpoints, connections = get_band_qpoints_and_path_connections([path], npoints=args.npoints)
 ph.run_band_structure(qpoints,with_eigenvectors=True,is_band_connection=True, path_connections=connections, labels=labels)
@@ -139,24 +124,6 @@ dist=band_dict['distances']  # dist is an array of segments array. Corresponds t
 freq=band_dict['frequencies']
 kpt=qpoints
 eigs=band_dict['eigenvectors']
-
-
-###print(freq[0][0][n])
-###displstrength(eigs[0][0].T[n].real,atomsetp,atomsetn,ph.primitive.get_masses())
-#f, e = ph.get_frequencies_with_eigenvectors(kpt[0][0])
-#print(f[0])
-
-#for i in range(len(ph.primitive.get_masses())):
-
-#   print("%s" % "".join("% 10.8f " % e[0][i*3+j] for j in range(3)))
-#displstrength(e[3].real,atomsetp,atomsetn,ph.primitive.get_masses())
-
-#dmat = ph.get_dynamical_matrix_at_q([0,0,0])
-#eigvals, egs = np.linalg.eigh(dmat)
-#e=egs.T
-
-
-#sys.exit(1)
 
 # Plotting routine
 params = {
@@ -169,14 +136,12 @@ params = {
 plt.rcParams.update(params)
 fig, ax = plt.subplots() 
 
-#mini=ceil(mini*10)/10
-#maxi=ceil(maxi*10)/10
 # Find min and max 
 mini=0
 maxi=0
-#z[segment][band num][k-point]
 intens=[[[0.0 for i in range(args.npoints)] for j in range(len(freq[0][0]))] for k in range(len(dist))]
 
+# Plotting the band
 # Iterate over each segment
 for i in range(len(dist)):
 # Iteraton over each band
@@ -197,10 +162,9 @@ else:
         mini=-abs(maxi)
     else:
         maxi=abs(mini)
-#mini=-1
-#maxi=1
 
 print('mini=%f, maxi=%f' % (mini,maxi))
+# Set manually minimal amd maximal color scale belo if needed by uncommenting the following 2 lines
 #mini=-1
 #maxi=1
 norm = plt.Normalize(mini, maxi)
@@ -226,20 +190,22 @@ for i in range(len(dist)):
         for j in range(len(dist[i])):
             x.append(dist[i][j]) # j-point in the i-th segment 
             y.append(freq[i][j][nbnd]) # frequency at j-point in the i-th segment for band nbnd
-#            z.append(displstrength(eigs[i][j].T[nbnd].real,atomsetp,atomsetn,ph.primitive.get_masses()))
-#        ax.plot(x, y, c='black', lw=1.0, alpha=0.5)                          `
+
         plt.scatter(x, y, c=z, cmap=cmap, marker='s', s=1, norm=norm)
 
 # Labels
-ax.set_ylabel(r'Frequency', fontsize=12)
+ax.set_ylabel(r'$\nu$, cm$^{-1}$', fontsize=12)
 ax.set_xlim([dist[0][0],dist[len(dist)-1][-1]])
+# Set y limit if set
+if (args.ylim):
+    ax.set_ylim(float(args.ylim.split()[0]),float(args.ylim.split()[1]))
 xticks=[dist[i][0] for i in range(len(dist))]
 xticks.append(dist[len(dist)-1][-1])
 ax.set_xticks(xticks)
 print(labels)
 ax.set_xticklabels(labels)
 ax.grid(which='major', axis='x', c='gray', linestyle='--', alpha=0.8)
-#ax.xticks(xticks,ticklabels)
+
 cbar=plt.colorbar(cax = fig.add_axes([0.92, 0.4, 0.02, 0.48]))
 cbar.ax.get_yaxis().set_ticks([])
 
@@ -262,22 +228,4 @@ else:
             cbar.ax.text(1.2, 0.0, args.cmlabels.split()[0], ha='left', va='center')
             cbar.ax.text(1.2, 1.0, args.cmlabels.split()[1], ha='left', va='center')
 
-#cbar.ax.text(1.2, 0.0, 'O', ha='left', va='center')
-#cbar.ax.text(1.2, 1.0, 'Al', ha='left', va='center')
-#plt.show()
 plt.savefig(args.out_fn, bbox_inches='tight')
-
-#Simple color map
-# plt.pyplot.scatter([1,2,3,4], [1,4,9,16], c=['r','g','b','black']) 
-
-
-
-
-#ph.run_band_structure(qpoints, path_connections=connections, labels=labels)
-#ph.plot_band_structure().savefig('band_part.pdf')
-
-#    api_phonopy.py
-#    def get_frequencies_with_eigenvectors(self, q):
-#    def get_dynamical_matrix_at_q(self, q):
-
-#ph.auto_band_structure(plot=True).savefig('band_part.pdf')
